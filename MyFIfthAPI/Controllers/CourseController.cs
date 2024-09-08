e27597808051191a5ac244df2714648e44f6ec22
P4vs <jhonpablotiquis30@gmail.com>
2024-09-08 12:55:56 +0800

    API

---
+++ MyFIfthAPI/Controllers/CourseController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstDapper.Model;
+using MyFirstDapper.Service;
+
+namespace MyFIfthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+
+        [HttpPost]
+        public bool AddCourse(CourseModel course)
+        {
+            CourseService courseService = new CourseService();
+            courseService.AddCourse(course);
+            return true;
+        }
+
+    }
+}
---
+++ MyFIfthAPI/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstDapper.Model;
+using MyFirstDapper.Service;
+
+namespace MyFIfthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpPost]
+        public bool AddStudent(StudentModel student)
+        {
+            studentService StudentService = new studentService();
+            StudentService.AddStudent(student);
+            return true;
+        }
+    }
+}
